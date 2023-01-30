4dbd4cd0920f615307e2c0990a0497430fd7d470
Himanshu <kumarhimanshu2219@gmail.com>
2023-01-30 13:28:59 +0530

    single state completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,21 @@ import './App.css'
 import Header from './components/Header'
 
 function App() {
-
-  const [form, setForm] = useState({});
+  const [form,setForm] = useState({ fName:"",lName:"",msg:"" });
   const [data, setData] = useState([]);
 
   const handleSubmit = () => {
     console.log(form);
-    if ((form.fName && form.lName && form.msg) === "") {
-      return
+    if((form.fName&& form.lName && form.msg)===""){
+      return  
     }
-    setData([form, ...data],)
-    setForm({ fName: "", lName: "", msg: "" })
+     setData([ form , ...data],)
+      setForm({ fName:"",lName:"",msg:""})
   }
 
   const handleDelete = (e) => {
-    const filterFun = data.filter(elem, index => e != index)
-    setData(filterFun)
+      const filterFun = data.filter( (elem,index) => e!=index)
+      setData(filterFun)
   }
 
   return (
@@ -30,20 +29,20 @@ function App() {
           placeholder='first name'
           className='input-text'
           value={form.fName}
-          onChange={(e) => setForm({ ...form, fName: e.target.value })}
+          onChange={(e) => setForm({...form,fName:e.target.value})}
         />
         <input
           type="text"
           placeholder='last name'
           className='input-text'
           value={form.lName}
-          onChange={(e) => setForm({ ...form, lName: e.target.value })}
+          onChange={(e) => setForm({...form,lName:e.target.value})}
         />
         <br />
         <textarea
           placeholder='message'
           value={form.msg}
-          onChange={(e) => setForm({ ...form, msg: e.target.value })}>
+          onChange={(e) => setForm({...form,msg:e.target.value})}>
         </textarea>
         <br />
         <input
@@ -52,23 +51,23 @@ function App() {
           className='submit-btn'
           onClick={handleSubmit}
         />
+        <input type="color" />
       </form>
       <div className='result-container'>
-        {
-          data.map((elem, i) => {
-            return (
-              <div key={i}>
-                <div className='result-box'>
-                  <h3 className='result-heading'>Name : {elem.fName + " " + elem.lName}</h3>
-                  <hr />
-                  <p className='result-para'>{elem.msg}</p>
-                </div>
-                <button className='delete-btn' onClick={() => handleDelete(i)}>DELETE</button>
+      {
+        data.map((elem, i) => {
+          return (
+            <div key={i}>
+              <div className='result-box'>
+                <h3 className='result-heading'>Name : {elem.fName + " " + elem.lName}</h3>
+                <hr />
+                <p className='result-para'>{elem.msg}</p>
               </div>
-
-            )
-          })
-        }
+              <button className='delete-btn' onClick={()=>handleDelete(i)}>DELETE</button>
+              </div>
+          )
+        })
+      }
       </div>
 
     </div>
