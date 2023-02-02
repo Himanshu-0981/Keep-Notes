fb665af4532d54d37a0fc1d2d950a29d4e5a6004
Himanshu <kumarhimanshu2219@gmail.com>
2023-02-02 18:13:46 +0530

    Toast feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,85 @@
-import React, { useState } from 'react';
-import './App.css'
-import Header from './components/Header'
-import Output from './components/Output';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Output from "./components/Output";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [form, setForm] = useState(
-    {
-      fName: '',
-      lName: '',
-      msg: '',
-      bgColor: '',
-      textColor: '',
-    });
+  const [form, setForm] = useState({
+    fName: "",
+    lName: "",
+    msg: "",
+    bgColor: "",
+    textColor: "",
+  });
 
   const [data, setData] = useState([]);
 
   const handleSubmit = () => {
-    if ((form.fName && form.lName && form.msg) === '') {
-      alert('input field is empty')
-      return
-    }else {
-      setData([form, ...data],)
-      setForm({ fName: "", lName: "", msg: "" })
+    if ((form.fName && form.lName && form.msg) === "") {
+      toast.error("input field is empty");
+      return;
+    } else {
+      setData([form, ...data]);
+      setForm({ fName: "", lName: "", msg: "" });
+      toast.success("data added");
     }
-  }
+  };
 
+  // /** @deprecated */
   const keyPress = (event) => {
-    if(event.key == 'Enter'){
-      handleSubmit()
+    if (event.key == "Enter") {
+      handleSubmit();
     }
-  }
+  };
 
   return (
     <div className="App">
       <Header />
+      <ToastContainer />
       <form onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
-          placeholder='first name'
-          className='input-text'
+          placeholder="first name"
+          className="input-text"
           value={form.fName}
           onChange={(e) => setForm({ ...form, fName: e.target.value })}
-          onKeyPress={(e)=>keyPress(e)}
+          onKeyPress={(e) => keyPress(e)}
         />
         <input
           type="text"
-          placeholder='last name'
-          className='input-text'
+          placeholder="last name"
+          className="input-text"
           value={form.lName}
           onChange={(e) => setForm({ ...form, lName: e.target.value })}
-          onKeyPress={(e)=>keyPress(e)}
+          onKeyPress={(e) => keyPress(e)}
         />
         <br />
         <textarea
-          placeholder='message'
+          placeholder="message"
           value={form.msg}
           onChange={(e) => setForm({ ...form, msg: e.target.value })}
-          onKeyPress={(e)=>keyPress(e)}
-          >
-        </textarea>
+          onKeyPress={(e) => keyPress(e)}
+        ></textarea>
         <br />
-        <div className='color-pallet-container'>
-          <div className='bg-color-pallet'>
+        <div className="color-pallet-container">
+          <div className="bg-color-pallet">
             <p>Choose Background color </p>
-            <input type="color"
+            <input
+              type="color"
               value={form.bgColor}
               onChange={(e) => setForm({ ...form, bgColor: e.target.value })}
-              onKeyPress={(e)=>keyPress(e)}
+              onKeyPress={(e) => keyPress(e)}
             />
           </div>
-          <div className='text-color-pallet'>
+          <div className="text-color-pallet">
             <p>Choose Text color </p>
-            <input type="color"
+            <input
+              type="color"
               value={form.textColor}
               onChange={(e) => setForm({ ...form, textColor: e.target.value })}
-              onKeyPress={(e)=>keyPress(e)}
+              onKeyPress={(e) => keyPress(e)}
             />
           </div>
         </div>
@@ -82,17 +87,15 @@ function App() {
         <input
           type="submit"
           value="Done"
-          className='submit-btn'
+          className="submit-btn"
           onClick={handleSubmit}
         />
       </form>
-      <div className='result-container'>
+      <div className="result-container">
         <Output data={data} state={setData} />
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,31 +1,38 @@
-import React from "react"
-import '../App.css'
+import React from "react";
+import "../App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Output = ({ data, state }) => {
+  const handleDelete = (e) => {
+    const filterFun = data.filter((elem, index) => e != index);
+    state(filterFun);
+    toast.success("data deleted");
+  };
 
-    const handleDelete = (e) => {
-        const filterFun = data.filter((elem, index) => e != index)
-        state(filterFun)
-    }
+  return (
+    <>
+      {data.map((elem, i) => {
+        return (
+          <div key={i}>
+            <div
+              className="result-box"
+              style={{ background: elem.bgColor, color: elem.textColor }}
+            >
+              <h3 className="result-heading">
+                Name : {elem.fName + " " + elem.lName}
+              </h3>
+              <hr />
+              <p className="result-para">{elem.msg}</p>
+            </div>
+            <button className="delete-btn" onClick={() => handleDelete(i)}>
+              DELETE
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-    return (
-        <>
-            {
-                data.map((elem, i) => {
-                    return (
-                        <div key={i}>
-                            <div className='result-box' style={{ background: elem.bgColor, color: elem.textColor }}>
-                                <h3 className='result-heading'>Name : {elem.fName + " " + elem.lName}</h3>
-                                <hr />
-                                <p className='result-para'>{elem.msg}</p>
-                            </div>
-                            <button className='delete-btn' onClick={() => handleDelete(i)}>DELETE</button>
-                        </div>
-                    )
-                })
-            }
-        </>
-    )
-}
-
-export default Output
+export default Output;
